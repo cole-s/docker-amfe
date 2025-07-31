9cbe4baeb356216798453086f9b11bbd39ea9e28
cole-s <schaarca@gmail.com>
2025-07-31 08:52:33 -0400

    starting with just microfrontend architecture

---
+++ app-workspace/apps/app-shell/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+
+    {
+        path: 'app-one',
+        loadComponent: () => import('@app-workspace/app-one/app.component').then(m => m.AppComponent)
+    },
+    {
+        path: 'app-two',
+        loadComponent: () => import('@app-workspace/app-two/app.component').then(m => m.AppComponent)
+    },
+
+];
