e3670b7670c168bec495398ec68fbe5c77ed8c82
cole-s <schaarca@gmail.com>
2025-08-07 14:22:14 -0400

    checkpoint

--- scripts/build-all.bat
+++ scripts/build-all.bat
@@ -4,4 +4,13 @@
 
 echo Skipping UI library build (consumbed directly by apps)...
 
-echo Building app-shell... && npx nx build app-shell --configuration=production && echo Building app-one... && npx nx build app-one --configuration=production && echo Building app-two... && npx nx build app-two --configuration=production && echo All builds completed!
+echo Building app-shell... 
+&& npx nx build app-shell --configuration=production 
+
+&& echo Building app-one... 
+&& npx nx build app-one --configuration=production 
+
+&& echo Building app-two... 
+&& npx nx build app-two --configuration=production 
+
+&& echo All builds completed!
